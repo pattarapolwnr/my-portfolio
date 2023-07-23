267bcc4df5dc3b2fb3148a28cffd54ac85953b0c
paopao2001 <pattarapol.wangnirun@gmail.com>
2023-07-23 21:10:06 +0700

    updated: image path

--- src/data/tools.js
+++ src/data/tools.js
@@ -1,8 +1,8 @@
 const tools = [
-  { id: 1, img: './assets/vscode_icon.png' },
-  { id: 2, img: './assets/intellij_icon.png' },
-  { id: 3, img: './assets/postman_icon.png' },
-  { id: 4, img: './assets/git_icon.png' },
+  { id: 1, img: '/assets/vscode_icon.png' },
+  { id: 2, img: '/assets/intellij_icon.png' },
+  { id: 3, img: '/assets/postman_icon.png' },
+  { id: 4, img: '/assets/git_icon.png' },
 ];
 
 export default tools;
